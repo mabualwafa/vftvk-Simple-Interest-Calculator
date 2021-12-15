03ec152bcf0aad73ef5d686037af06cdc5afebb2
Mohammad.AbuAlwafa <Mohammad.AbuAlwafa@HQ-IT-1528-LP>
2021-12-15 22:03:35 +0200

    fix

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ function compute()
        var year = new Date().getFullYear()+parseInt(years);
 
        if(principal ==undefined || principal <=0) {
+      
+        alert('Enter a positive number');
         document.getElementById("principal").focus();
-        alert('Enter a positive number')
     }
     else {
     document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>" 
